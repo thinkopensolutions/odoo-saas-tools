93fe3a3c550f98781853148405e7bb2c0a674cce
purnendu03 <purnendusingh1985@gmail.com>
2017-05-02 19:09:31 +0530

    [ADD] A new module with TKO access rights

---
+++ tko_saas_access_right/__manifest__.py
@@ -0,0 +1,20 @@
+# *- c0d1ng: u7f-8 -*-
+# OpenERP, Open Source Management Solution
+# Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be&gt;).
+# Thinkopen - Brasil
+# Copyright (C) Thinkopen Solutions (<http://www.thinkopensolutions.com.br&gt;)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    'name': 'TKo SaaS Access Rights',
+    'category': 'Tko saas',
+    'summary': 'Add additional access rights to SaaS module',
+    'website': 'www.geminatecs.com',
+    'author' : 'Geminate Consultancy Services',
+    'depends': ['saas_portal'],
+    'data': [
+        'security/ir.model.access.csv',
+             ],
+    'qweb': [],
+    'installable': True,
+    'auto_install': False,
+}
